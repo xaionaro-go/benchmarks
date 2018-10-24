526d132c3c6a496aa98313e14d79bd0c27b5b3ea
Dmitry Yu Okunev <xaionaro@wisebits.com>
2018-10-24 13:55:10 +0300

    updated "defer"

--- defer/README.md
+++ defer/README.md
@@ -1,4 +1,5 @@
 ```
-BenchmarkDefer_1goroutine-4     	30000000	        53.5 ns/op
-BenchmarkNoDefer_1goroutine-4   	2000000000	         1.94 ns/op
+BenchmarkDefer-4        30000000                52.2 ns/op             0 B/op          0 allocs/op
+BenchmarkTwoDefers-4    20000000                91.3 ns/op             0 B/op          0 allocs/op
+BenchmarkNoDefer-4      2000000000               1.96 ns/op            0 B/op          0 allocs/op
 ```
--- defer/defer_test.go
+++ defer/defer_test.go
@@ -13,14 +13,27 @@ func withDefer() (r uint64) {
 	return
 }
 
+func withTwoDefers() (r uint64) {
+	defer func() {
+		r = 1
+	}()
+	defer func() {
+	}()
+	return
+}
+
 func noDefer() uint64 {
 	return 1
 }
 
-func BenchmarkDefer_1goroutine(b *testing.B) {
+func BenchmarkDefer(b *testing.B) {
 	helpers.Benchmark(b, withDefer)
 }
 
-func BenchmarkNoDefer_1goroutine(b *testing.B) {
+func BenchmarkTwoDefers(b *testing.B) {
+	helpers.Benchmark(b, withTwoDefers)
+}
+
+func BenchmarkNoDefer(b *testing.B) {
 	helpers.Benchmark(b, noDefer)
 }
