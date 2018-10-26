d9c534f639d66cac2dfd0495210c840b77924309
Dmitry Yu Okunev <xaionaro@wisebits.com>
2018-10-26 13:39:30 +0300

    Added assign/map

---
+++ assign/README.md
@@ -0,0 +1,4 @@
+```
+BenchmarkDummy-4       	1000000000	         1.95 ns/op	       0 B/op	       0 allocs/op
+BenchmarkAssignMap-4   	1000000000	         2.64 ns/op	       0 B/op	       0 allocs/op
+```
---
+++ assign/map_test.go
@@ -0,0 +1,34 @@
+package bench
+
+import (
+	"testing"
+
+	"github.com/xaionaro-go/benchmarks/internal/helpers"
+)
+
+var a = map[string]interface{}{
+	"a": "a",
+	"b": "b",
+	"c": "c",
+	"1": 1,
+	"i": map[string]interface{}{},
+}
+var b = map[string]interface{}{}
+
+func dummy() uint64 {
+	return 0
+}
+
+func assignMap() uint64 {
+	b = a
+	return 0
+}
+
+func BenchmarkDummy(b *testing.B) {
+	helpers.Benchmark(b, dummy)
+}
+
+func BenchmarkAssignMap(b *testing.B) {
+	helpers.Benchmark(b, assignMap)
+}
+
