645776ceb6364b2e1017ecd102e00b4ecb7b2a00
Dmitry Yu Okunev <xaionaro@wisebits.com>
2019-02-14 16:23:52 +0200

    Updated the replacer

--- replacer/README.md
+++ replacer/README.md
@@ -1,4 +1,5 @@
 ```
-BenchmarkReplace-4      10000000               150 ns/op              64 B/op          4 allocs/op
-BenchmarkReplacer-4      2000000               803 ns/op            1008 B/op         13 allocs/op
+BenchmarkReplace-4              10000000               217 ns/op             256 B/op          4 allocs/op
+BenchmarkReplacer-4              1000000              1507 ns/op            1200 B/op         13 allocs/op
+BenchmarkBytesReplace-4          5000000               234 ns/op             192 B/op          3 allocs/op
 ```
--- replacer/benchmark_test.go
+++ replacer/benchmark_test.go
@@ -1,25 +1,39 @@
 package replacer
 
 import (
+	"bytes"
 	"testing"
 	"strings"
 )
 
 func BenchmarkReplace(b *testing.B) {
+	s := `loooooooooooooooooooooooooooooooooooooooooooooong input string`
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			strings.Replace(strings.Replace(strings.Replace(`input string`, `one`, `two`, -1), `inp`, `nimb`, -1), `ring`, `grok`, -1)
+			strings.Replace(strings.Replace(strings.Replace(s, `one`, `two`, -1), `inp`, `nimb`, -1), `ring`, `grok`, -1)
 		}
 	})
 }
 
 func BenchmarkReplacer(b *testing.B) {
+	s := `loooooooooooooooooooooooooooooooooooooooooooooong input string`
 	b.ResetTimer()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
 			replacer := strings.NewReplacer(`one`, `two`, `inp`, `nimb`, `ring`, `grok`)
-			replacer.Replace(`input string`)
+			replacer.Replace(s)
 		}
 	})
 }
+
+func BenchmarkBytesReplace(b *testing.B) {
+	bs := []byte(`loooooooooooooooooooooooooooooooooooooooooooooong input string`)
+	b.ResetTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			bytes.Replace(bytes.Replace(bytes.Replace(bs, []byte(`one`), []byte(`two`), -1), []byte(`inp`), []byte(`nimb`), -1), []byte(`ring`), []byte(`grok`), -1)
+		}
+	})
+}
+
