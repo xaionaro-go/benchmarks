22ab4351eba543af3a080f614942b80f22dcf154
Dmitry Yu Okunev <xaionaro@dx.center>
2019-09-09 23:35:26 +0300

    More tests for a channel

--- chan/README.md
+++ chan/README.md
@@ -1,6 +1,11 @@
+```
 goos: linux
 goarch: amd64
 pkg: github.com/xaionaro-go/benchmarks/chan
-BenchmarkChan-8   	 4992451	       246 ns/op	       0 B/op	       0 allocs/op
+BenchmarkChan1-8                	 5229620	       239 ns/op	       0 B/op	       0 allocs/op
+BenchmarkChan2-8                	 7536854	       188 ns/op	       0 B/op	       0 allocs/op
+BenchmarkChan1024-8             	11750379	        98.9 ns/op	       0 B/op	       0 allocs/op
+BenchmarkChan1024WithSelect-8   	 5879042	       178 ns/op	       0 B/op	       0 allocs/op
 PASS
-ok  	github.com/xaionaro-go/benchmarks/chan	1.476s
+ok  	github.com/xaionaro-go/benchmarks/chan	5.591s
+```
--- chan/chan_test.go
+++ chan/chan_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func BenchmarkChan(b *testing.B) {
+func BenchmarkChan1(b *testing.B) {
 	ch := make(chan struct{})
 
 	go func() {
@@ -16,5 +16,54 @@ func BenchmarkChan(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		ch <- struct{}{}
+	}
+}
+
+func BenchmarkChan2(b *testing.B) {
+	ch := make(chan struct{}, 2)
+
+	go func() {
+		for {
+			<-ch
+		}
+	}()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ch <- struct{}{}
+	}
+}
+
+func BenchmarkChan1024(b *testing.B) {
+	ch := make(chan struct{}, 1024)
+
+	go func() {
+		for {
+			<-ch
+		}
+	}()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ch <- struct{}{}
+	}
+}
+
+func BenchmarkChan1024WithSelect(b *testing.B) {
+	ch0 := make(chan struct{}, 1024)
+	ch1 := make(chan struct{}, 1024)
+
+	go func() {
+		for {
+			select {
+			case <-ch0:
+			case <-ch1:
+			}
+		}
+	}()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ch0 <- struct{}{}
 	}
 }
